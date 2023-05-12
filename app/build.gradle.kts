3789bb36ef9ca96f047b83ad1414a0563bcdb5ff
shidobecker <juliowebdeveloper@hotmail.com>
2023-05-12 16:30:31 -0300

    Update libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,11 +51,11 @@ android {
 }
 
 dependencies {
-    implementation ("androidx.appcompat:appcompat:1.4.2")
+    implementation ("androidx.appcompat:appcompat:1.6.1")
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.activity:activity-compose:1.7.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -65,9 +65,10 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
 
     implementation("com.vanniktech:android-image-cropper:4.5.0")
-    implementation("io.coil-kt:coil-compose:2.2.2")
+    implementation("io.coil-kt:coil-compose:2.3.0")
     implementation("io.coil-kt:coil-gif:2.2.2")
     implementation ("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+    testImplementation("com.google.truth:truth:1.1.3")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
